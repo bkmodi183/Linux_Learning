7499da0899cec5478a07376c18525f37898192d3
Bhavesh <bhavesh123@gmail.com>
2025-12-21 16:11:31 +0000

    added backup shell script

---
+++ bash_scripts/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+<<info
+
+This script will take periodic backups
+
+info
+
+src=$1
+dest=$2
+
+timestamp=$(date '+%Y-%m-%d-%H-%M')
+
+zip -r "$dest/backup-$timestamp.zip" $src > /dev/null
+
+echo "Backup completed"
+
